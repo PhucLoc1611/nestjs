2060b20bb73b6a7a75d6c89ed472764118362c36
locgnguyenphuc@gmail.com <locgnguyenphuc@gmail.com>
2025-10-26 14:30:37 +0700

    crud

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postService: PostsService) {}
+  @Get()
+  getPosts() {
+    return this.postService.getPosts();
+  }
+  @Post()
+  createPost(@Body() body: any) {
+    return this.postService.createPost(body);
+  }
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return this.postService.getPost(id);
+  }
+  @Put(':id')
+  updatePost(@Body() body: any, @Param('id') id: string) {
+    return this.postService.UpdatePost(id, body);
+  }
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    return this.postService.deletePost(id);
+  }
+}
